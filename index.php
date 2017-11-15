c64ece498252ac5b3ad1d4003daaa0370e1bbece
jdonald32 <1640528+jdonald32@users.noreply.github.com>
2017-11-14 21:00:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if ($con->connect_errno) {
 ?>
 <br /><br /><img src="images/hormonelogo.jpg" height="100" width="auto" />
    <!--- <img src="images/dummylogo.png" style="margin:-15px;"/>-->
-<h3>Use this interface to query, view, and download hormone and life history data yeah. </h3>
+<h3>Use this interface to query, view, and download hormone and life history data. </h3>
 <!--<h3><span style="color:red">Dmata unverified: for database testing only!</span></h3>-->
 <div style="float:right;padding-right:100px;border:1px black;width:600px;"><strong>Change Log:<br />11/13/2017: Current dataset as of Nov. 13, 2017.</strong><br />
   </div>
